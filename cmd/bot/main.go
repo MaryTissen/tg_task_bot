d77a6de206089d0e873eb47d82fee9c790c18cbd
Маша Тиссен <mashatissen@MacBook-Pro-Masa.local>
2023-04-13 23:11:27 +0300

    Main

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("5906294458:AAErtRYQ6OprFjfRt0Kvcw1UhIuF7n14xT0")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	updateConfig := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates, err := bot.GetUpdatesChan(updateConfig)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		switch update.Message.Command() {
+		case "help":
+			helpCommand()
+		case "new":
+			newCommand()
+		case "edit":
+			editCommand()
+		case "delete":
+			deleteCommand()
+		case "get":
+			getCommand()
+		case "list":
+			listCommand()
+		}
+	}
 }
